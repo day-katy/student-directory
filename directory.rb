7ae2911f55972ecc5c82a47d90d78b88c4c27753
Katy Day <day.katy@gmail.com>
2020-12-15 16:10:38 +0000

    Take away name beginning with H, added only printing name if less than 12 characters

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ end
 
 def print(students)
   students.each do |student|
-    if student[:name].start_with? "H"
+    if student[:name].length < 12
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     else
       return nil
