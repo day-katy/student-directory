8e9c3d4b84be89a4138c551b69db9a63a6742f17
Katy Day <day.katy@gmail.com>
2020-12-15 15:58:13 +0000

    Adding index numbers to students

--- directory.rb
+++ directory.rb
@@ -20,9 +20,10 @@ def print_header
   puts "The studetns of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
-  students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+  puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
