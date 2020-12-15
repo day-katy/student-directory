eb2dfe8214db1030d9835b83f6217cfed7e1e988
Katy Day <day.katy@gmail.com>
2020-12-15 16:08:33 +0000

    Take away indexing and making it only print student names beginning with the letter H

--- directory.rb
+++ directory.rb
@@ -22,10 +22,15 @@ def print_header
 end
 
 def print(students)
-  students.each.with_index(1) do |student, index|
-  puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    if student[:name].start_with? "H"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    else
+      return nil
+    end
   end
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
