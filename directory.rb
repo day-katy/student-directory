359c027818bd41d2012f62d0abb4f747d9137c7a
Katy Day <day.katy@gmail.com>
2020-12-17 16:11:24 +0000

    Two students directories now, the test has the 'interactive menu'

--- directory.rb
+++ directory.rb
@@ -1,8 +1,10 @@
+@students = []
+
 def input_students
   months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
   puts "Please enter the names of the students, and the cohorts they're in, separated by a comma".center(30)
   puts "To finish, just hit return twice".center(30)
-  students = []
+  @students = []
   name_cohort = gets.chomp
   while !name_cohort.empty? do
     name, cohort = name_cohort.split(", ")
@@ -21,7 +23,6 @@ def input_students
     name_cohort = gets.chomp
   end
 end
-  students
 end
 
 def print_header
@@ -29,7 +30,7 @@ def print_header
   puts "-------------".center(30)
 end
 
-def print(names)
+def print_students_list(names)
   puts "Which cohort would you like to see?"
   answer = gets.chomp.to_sym.capitalize
   sort_by_cohort = {}
@@ -45,39 +46,47 @@ def print(names)
   puts sort_by_cohort[answer]
 end
 
-def print_footer(students)
-  if students.count == 1
+def print_footer(@students)
+  if @students.count == 1
     puts "Overall, we have #{students.count} great student".center(30)
   elsif
   puts "Overall, we have #{students.count} great students".center(30)
   end
 end
 
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    # input the students
+    @students = input_students
+  when "2"
+    # show the students
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
 end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 
 interactive_menu
